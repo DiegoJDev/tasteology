5bd1f8e187c818c0d31c96483762eb366677198a
Diego Cardenas Dev <diegocardenas.developer@gmail.com>
2025-09-17 11:53:30 -0500

    adding header/navbar/footer

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import './styles/main.scss';
 import Gallery from './modules/Gallery';
 import { initCards } from './modules/Cards';
+import { Navigation } from './modules/Navigation';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Initialize Navigation
+  new Navigation();
+
+  // Initialize Gallery
   document.querySelectorAll<HTMLElement>('[data-module="gallery"]').forEach(async (el) => {
     const mod = new Gallery(el);
     await mod.init();
   });
 
+  // Initialize Cards
   initCards();
 });
